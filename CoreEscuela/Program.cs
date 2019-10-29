1fecd4102e5f286aeda737ad28c1774072b97ce3
mateo henao <m-teo770@hotmail.com>
2019-10-28 22:49:56 -0500

    Introduccion POO

---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class CoreEscuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo = "teo";
+
+        public void Timbrar()
+        {
+            //Todo
+            Console.WriteLine("timbrando");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new CoreEscuela();
+            miEscuela.nombre = "teoSchool";
+            miEscuela.direccion = "calarca";
+            miEscuela.añoFundacion = 2019;
+            Console.WriteLine("timbrar");
+            miEscuela.Timbrar();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
